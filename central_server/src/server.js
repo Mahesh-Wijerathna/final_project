491abcc8b948b50f32f9911b2bd366ff21bb29ea
Mahesh-Wijerathna <maheshtharanga49@gmail.com>
2024-03-16 10:55:24 +0530

    central only

--- central_server/src/server.js
+++ central_server/src/server.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const proxy = require('express-http-proxy');
 const verify = require('./verify');
 
-
+const port = process.env.PORT || 10000;
 const app = express();
 app.use(cors());
 
@@ -11,7 +12,7 @@ app.use(cors());
 
 
 
-app.use('/auth'       , proxy('http://localhost:4001'));
+app.use('/auth'       , proxy('https://final-project-xp60.onrender.com:10001'));
 app.use('/tourist'    , proxy('http://localhost:4002'));
 app.use('/m_center'   , proxy('http://localhost:4003'));
 app.use('/destination', proxy('http://localhost:4004'));
@@ -19,6 +20,6 @@ app.use('/appointment', proxy('http://localhost:4005'));
 app.use('/',(req,res,next) => {
     return res.status(200).json({message: 'Welcome to the central server'});
 });
-app.listen(4000, () => {
-    console.log('Central server running on port 4000 ...');
+app.listen(port, () => {
+    console.log(`Central server running on port ${port} ...`);
 });
